648f4fd5cd23405dd6ad9ffcd55daa0a971209d6
cs2r <centerofroboticsg108@gmail.com>
2022-09-27 15:48:44 +0300

    first commit

---
+++ autostart2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+. "/opt/ros/kinetic/setup.sh"
+. "/home/powerbot/driver_ws/devel/setup.sh"
+
+export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_IP=10.42.0.1
+#roslaunch powerbot start_laser.launch
